cfb3ab589e9df590ed0eb8fba62ec781998d24ba
李强 <liqiang@game7.cc>
2026-05-19 16:14:16 +0800

    #1.7.2

--- Package.swift
+++ Package.swift
@@ -5,26 +5,32 @@ import PackageDescription
 
 let package = Package(
     name: "YF_Core",
-    platforms: [
-        .iOS(.v12)
-    ],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "YF_Core",
-            targets: ["YF_Core"]
+            targets: ["YF_Core_Target"]
         ),
     ],
     dependencies: [
         .package(
-	        url: "https://github.com/leonard-li/YF_Utils_SPM.git", 
-	        exact: "1.2.7"
-	)
+            name: "YF_Utils",
+            url: "https://github.com/leonard-li/YF_Utils_SPM.git", 
+            .from("1.2.9")
+    )
     ],
     targets: [
+        .target(
+        name: "YF_Core_Target",
+        dependencies: [
+            "YF_Core_Binary",
+            .product(name: "YF_Utils", package: "YF_Utils_SPM")
+        ]
+        ),
         .binaryTarget(
-            name: "YF_Core",
-            url: "https://mvn.yifants.cn/artifactory/fineboost-spm/core/1.7.1/YFCore.xcframework.zip",
-            checksum: "cf8cecde371e499390e6f7a8236707f8ca0aa7dc4b5540329d217b5b4565e6c2"
+            name: "YF_Core_Binary",
+            url: "https://mvn.yifants.cn/artifactory/fineboost-spm/core/1.7.2/YFCore.xcframework.zip",
+            checksum: "5736d0dee9b8eef3d044d6b368aefcd4db92114df5d0deab7323f2ce16a45b26"
         )
     ]
 )
